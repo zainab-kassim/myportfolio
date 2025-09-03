1eb541f5d3bd19338e7eb1f2f8b599855ea97ab6
Zainab Kassim Temitope <kassimzainab11@gmail.com>
2025-09-03 19:33:55 +0100

    herosection.tsx updated

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -25,20 +25,20 @@ export default function HeroSection() {
       </div>
 
 
-      <div className='flex flex-col sm:px-14 px-5  justify-center items-center pt-5 mt-20 sm:mt-36'>
+      <div className='flex flex-col sm:px-14 px-5  justify-center items-center pt-5 mt-24 sm:mt-36'>
         <Image height={170} width={170} className='animate-fade-in-up w-21 h-21 ' quality={100} alt='memoji' src={memoji} />
         <div className='text-center text-white animate-fade-in-up font-bold font-sans mt-2  text-xl md:text-3xl lg:text-[41px]'>
           <h2 className='flex'>Zainab is a Software Developer </h2>
           <h2 className=''>From Lagos, <span className='text-green-700'>Nig</span>e<span className='text-green-700'>ria</span></h2>
         </div>
 
-        <ScrollReveal animation='animate-expand-horizontally' duration='800'>
+        <ScrollReveal animation='animate-expand-horizontally'>
           <div className='flex justify-items-center mt-8 gap-3 relative mb-14  '>
             <a href="https://x.com/TemiDevtips" target='_blank' className='cursor-pointer'><Image height={70} width={70} className='bg-[#292929]  sm:w-14 sm:h-12 w-13 h-11 rounded-md px-4 py-3 -rotate-3 relative top-1' alt='twitter logo' src='https://img.icons8.com/?size=100&id=A4DsujzAX4rw&format=png&color=ffffff' /></a>
             <a href="https://github.com/zainab-kassim" target='_blank' className='cursor-pointer'><Image height={70} width={70} className='bg-[#292929] sm:w-14 sm:h-12 w-13 h-11 rounded-md px-4 py-3 -rotate-2' alt='githublogo' src={githublogo} /></a>
             <a href="https://www.linkedin.com/in/zainab-kassim/" target='_blank' className='cursor-pointer'><Image height={70} width={70} className='bg-[#292929] sm:w-14 sm:h-12 w-13 h-11 rounded-md px-4 py-3' alt='linkedinlogo' src='https://img.icons8.com/?size=100&id=2EqeH19eMd3a&format=png&color=ffffff' /></a>
             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=kassimzainab11@gmail.com" target="_blank" rel="noopener noreferrer"><Image height={70} width={70} className='bg-[#292929] sm:w-14 sm:h-12 w-13 h-11 rounded-md px-4 py-3 rotate-2' alt='maillogo' src='https://img.icons8.com/?size=100&id=Y2GfpkgYNp42&format=png&color=ffffff' /></a>
-            <a href="https://www.instagram.com/zainab_temi_/" target='_blank' className='cursor-pointer'><Image height={70} width={70} className='bg-[#292929] sm:w-14 sm:h-12 w-13 h-11 rounded-md px-4 py-3 rotate-3 relative top-1' alt='instagram logo' src='https://img.icons8.com/?size=100&id=32320&format=png&color=FFFFFF' /></a>
+            <a href="https://www.instagram.com/zainab_temi_/" target='_blank' className='cursor-pointer'><Image height={80} width={80} className='bg-[#292929] sm:w-14 sm:h-12 w-13 h-11 rounded-md px-4 py-3 rotate-3 relative top-1' alt='instagram logo' src='https://img.icons8.com/?size=100&id=32320&format=png&color=FFFFFF' /></a>
           </div>
         </ScrollReveal>
 
