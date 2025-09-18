4849b74117355000a16c1494350265c7cd6a2bee
Zainab Kassim Temitope <kassimzainab11@gmail.com>
2025-09-18 02:49:30 +0100

    project section updated

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -12,6 +12,7 @@ export default function Project() {
                 "Celstique is an e-commerce platform built to provide a seamless online shopping experience. It features a modern, responsive, and user-friendly interface that allows customers to easily browse products, manage their carts, and complete secure checkouts.",
             techStacks: ["React", "Next.js", "Tailwind", "Mongo Db", "Postman", "Typescript", "Express"],
             link: "https://celestique.vercel.app/",
+            github:"https://github.com/somkenemuscle/Celestique"
         },
         {
             title: "Appvarsity",
@@ -19,6 +20,7 @@ export default function Project() {
                 "Appvarsity is a learning platform designed to support Aptech students in their academic journey. It provides access to study materials, exam preparation resources, and essential information about Aptech courses.",
             techStacks: ["React", "Next.js", "Tailwind", "Mongo Db", "Postman", "Typescript", "Express"],
             link: "https://appvarsity.vercel.app/",
+            github:"https://github.com/somkenemuscle/AppVarsity"
         },
     ]
     return (
@@ -30,18 +32,18 @@ export default function Project() {
                 </div>
             </div>
 
-            <div className='grid md:grid-cols-2 grid-cols-1 gap-8 items-center mt-24'>
+            <div className='grid md:grid-cols-2 grid-cols-1 gap-8 items-stretch mt-24'>
                 {projects.map((project, index) => (
-                    <div key={index} className='bg-[#151515] border-[0.5px] px-8 pt-4 pb-8 rounded-lg border-[#313030]'>
-                        <div className='flex flex-row justify-between items-center'>
-                            <h2 className='text-2xl'>{project.title}</h2>
-                            <a href=""><Image height={70} width={70} className=' w-10 h-10 mt-5' alt='githublogo' src='https://img.icons8.com/?size=100&id=fmFqQmR0UdsR&format=png&color=480891' /></a>
+                    <div key={index} className='bg-[#151515] border-[0.5px] md:px-8 px-6 pt-4 pb-8 rounded-lg border-[#313030]'>
+                        <div className='flex flex-row  items-center justify-between'>
+                            <div className='text-2xl'>{project.title}</div>
+                            <a href={project.github}><Image height={70} width={70} className=' w-10 h-10' alt='githublogo' src='https://img.icons8.com/?size=100&id=fmFqQmR0UdsR&format=png&color=480891' /></a>
                         </div>
                         <hr className='mb-5 mt-2' />
                         <div className='text-[#ACA6A6] text-sm'>
                             {project.description}
                         </div>
-                        <div className='flex flex-row flex-wrap gap-2 text-sm mt-4 mb-5 items-center'>
+                        <div className='flex flex-row flex-wrap gap-2 text-sm mt-4 mb-8 items-center'>
                             {project.techStacks.map((techStack, i) => (
                                 <h2 key={i}>{techStack}.</h2>
                             ))}
